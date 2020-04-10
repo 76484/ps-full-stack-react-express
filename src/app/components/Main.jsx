03e027a8726faa48cd995d9ed60c44017fea9eb3
Steven Smith <steven.76484@gmail.com>
2020-04-09 22:24:26 -0400

    Adding route for /dashboard

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import { ConnectedDashboard } from "./Dashboard";
 import { store } from "../store";
+import { history } from "../store/history";
 
 export const Main = () => (
-  <Provider store={store}>
-    <ConnectedDashboard />
-  </Provider>
+  <Router history={history}>
+    <Provider store={store}>
+      <Route exact to="/dashboard" render={() => <ConnectedDashboard />} />
+    </Provider>
+  </Router>
 );
