7785987ea5c4b12cbfc877b842a450617509ac51
Steven Smith <steven.76484@gmail.com>
2020-04-10 14:31:02 -0400

    Adding updateTask endpoint to API

--- src/server/server.js
+++ src/server/server.js
@@ -17,9 +17,33 @@ export const addNewTask = async (task) => {
   await collection.insertOne(task);
 };
 
+export const updateTask = async (task) => {
+  const { id, group, isComplete, name } = task;
+  const db = await connectDB();
+  const collection = db.collection("tasks");
+
+  if (group) {
+    await collection.updateOne({ id }, { $set: { group } });
+  }
+
+  if (name) {
+    await collection.updateOne({ id }, { $set: { name } });
+  }
+
+  if (isComplete !== undefined) {
+    await collection.updateOne({ id }, { $set: { isComplete } });
+  }
+};
+
 app.post("/task/new", async (req, res) => {
   const task = req.body.task;
   await addNewTask(task);
+  res.status(200).send();
+});
+
+app.post("/task/update", async (req, res) => {
+  const task = req.body.task;
+  await updateTask(task);
   res.status(200).send();
 });
 
