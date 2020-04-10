585db194049cd7b3344617b6c8b0a2dc006342c9
Steven Smith <steven.76484@gmail.com>
2020-04-09 22:30:22 -0400

    Adding navigation link to /dashboard

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -3,13 +3,17 @@ import { Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import { ConnectedDashboard } from "./Dashboard";
+import { ConnectedNavigation } from "./Navigation";
 import { store } from "../store";
 import { history } from "../store/history";
 
 export const Main = () => (
   <Router history={history}>
     <Provider store={store}>
-      <Route exact to="/dashboard" render={() => <ConnectedDashboard />} />
+      <>
+        <ConnectedNavigation />
+        <Route exact to="/dashboard" render={() => <ConnectedDashboard />} />
+      </>
     </Provider>
   </Router>
 );
