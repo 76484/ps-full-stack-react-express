7884b06377484aaa2918fbd80a336bcf60573627
Steven Smith <steven.76484@gmail.com>
2020-04-10 21:05:55 -0400

    Removing unused reference

--- src/app/index.js
+++ src/app/index.js
@@ -2,6 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { Main } from "./components/Main";
-import { store } from "./store";
 
 ReactDOM.render(<Main />, document.getElementById("app"));
